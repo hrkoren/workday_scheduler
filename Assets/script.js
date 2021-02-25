f76d113906f438f8068d203c96c528dd0d5b89e5
HRush <hrkoren@gmail.com>
2021-02-25 18:24:40 -0500

    pushing finished code

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,15 @@
 var today = moment();
 $('#currentDay').text(today.format("MMM Do, YYYY"));
 
+//pull saved info from local storage
+//check current time and color-code text areas based on time
 function timeFunction() {
 var currentTime = moment().hour();
 console.log(currentTime);
-$('.time-block').each(function(){
+$('.time-block').each(function(index){
     var timeHour = parseInt($(this).attr('id').split('-')[1]);
     console.log(timeHour, currentTime);
+    $('#hour-' + timeHour + ' .description').val(localStorage.getItem('hour-' + timeHour));
     if (timeHour === currentTime) {
         $(this).removeClass('past');
         $(this).addClass('present');
@@ -20,28 +23,14 @@ $('.time-block').each(function(){
     }
 });
 }
-// text entry into plannerBlock and save to local storage upon save
+// text entry into text area and save to local storage upon save
 $('.saveBtn').on('click', function(){
-    console.log($(this));
+   console.log($(this));
    var textArea = $(this).siblings('.description').val();
    console.log(textArea);
    var currentHour = $(this).parent().attr('id');
    console.log(currentHour);
    localStorage.setItem(currentHour, textArea);
 })
-
-// $(window).on('load', function(){
-//     var textArea = $(this).parent('id').val('.description');
-//     // var textArea = parseInt($(this).attr('id'));
-//     console.log(textArea);
-//     var currentHour = $(this).children().attr('id');
-//     console.log(currentHour);
-//     localStorage.getItem(currentHour, textArea);
-// })
-
-
-//this works, but would have to do it for each hour
-//want to do for all at once
-$('#hour-8 .description').val(localStorage.getItem('hour-8'));
 
 timeFunction()
