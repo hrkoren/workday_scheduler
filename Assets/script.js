a82512799479dd8e0ede11fd31f63c8f94977c31
HRush <hrkoren@gmail.com>
2021-02-24 21:03:50 -0500

    pushing updated code

--- Assets/script.js
+++ Assets/script.js
@@ -1,27 +1,47 @@
 var today = moment();
 $('#currentDay').text(today.format("MMM Do, YYYY"));
 
-// function timeFunction() {
-// var currentTime = moment().hour();
-// var hour = document.querySelectorAll('#hour8 #hour9 #hour10 #hour11 #hour12 #hour13 #hour14 #hour15 #hour16 #hour17');
-// var plannerBlock = document.getElementsByTagName('.time-block');
-
-// if (hour === currentTime) {
-// plannerBlock.setAttribute('class', '.present');
-
-//  } else  (hour < currentTime) 
-//     plannerBlock.setAttribute('class', '.future');
-// }
-
+function timeFunction() {
 var currentTime = moment().hour();
-var currentHour = document.querySelectorAll('#hour8 #hour9 #hour10 #hour11 #hour12 #hour13 #hour14 #hour15 #hour16 #hour17');
-var plannerBlock = document.getElementsByClassName('.description');
-var pastTime = document.getElementsByClassName('.past');
-if (currentHour === currentTime) {
-    plannerBlock.classname = '.present';
-} else (currentHour > currentTime) 
-    plannerBlock.classname = '.future';
+console.log(currentTime);
+$('.time-block').each(function(){
+    var timeHour = parseInt($(this).attr('id').split('-')[1]);
+    console.log(timeHour, currentTime);
+    if (timeHour === currentTime) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+    } else if (timeHour > currentTime) {
+        $(this).removeClass('past');
+        $(this).addClass('future');
+    } else  {
+        $(this).removeClass('present');
+        $(this).removeClass('future');
+        $(this).addClass('past');
+    }
+});
+}
+// text entry into plannerBlock and save to local storage upon save
+$('.saveBtn').on('click', function(){
+    console.log($(this));
+   var textArea = $(this).siblings('.description').val();
+   console.log(textArea);
+   var currentHour = $(this).parent().attr('id');
+   console.log(currentHour);
+   localStorage.setItem(currentHour, textArea);
+})
+
+// $(window).on('load', function(){
+//     var textArea = $(this).parent('id').val('.description');
+//     // var textArea = parseInt($(this).attr('id'));
+//     console.log(textArea);
+//     var currentHour = $(this).children().attr('id');
+//     console.log(currentHour);
+//     localStorage.getItem(currentHour, textArea);
+// })
 
 
-//text entry into plannerBlock and save to local storage upon save
+//this works, but would have to do it for each hour
+//want to do for all at once
+$('#hour-8 .description').val(localStorage.getItem('hour-8'));
 
+timeFunction()
